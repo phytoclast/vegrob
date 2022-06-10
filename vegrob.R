f6ee3d04471650bbdc608e2cfba32448e1b34f8b
Schmidt <12001001973813@FEDIDCARD.GOV>
2022-06-10 12:43:21 -0400

    Update vegrob.R

--- vegrob.R
+++ vegrob.R
@@ -3,45 +3,6 @@ library(grid)
 library(gggrid)
 library(svgparser)
 
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Loat SVG as a grob
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-rlogo_url <- 'https://www.r-project.org/logo/Rlogo.svg'
-g <- svgparser::read_svg(rlogo_url)
-g$vp <- grid::viewport(x = 0.8, y = 0.8, width = unit(.4, 'npc'), height = unit(.4, 'snpc'))
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Define a callfback function to use within each panel.
-# See 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-func <- function(data, coords) {
-  
-  position_grob <- function(row) {
-    gnew <- g
-    gnew$vp$x      <- unit(row[['x']], 'npc')
-    gnew$vp$y      <- unit(row[['y']], 'npc')
-    gnew$vp$width  <- unit(row[['size']]/40, 'npc')
-    gnew$vp$height <- unit(row[['size']]/40, 'npc')
-    gnew$name <- strftime(Sys.time(), "%H%M%OS6") # Enforce unique name per grob. 
-    gnew
-  }
-  
-  grobs <- lapply(seq(nrow(coords)), function(idx) {position_grob(coords[idx, ])})
-  do.call(grid::grobTree, grobs)
-}
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Plot wih a `gggrid::grid_panel()`
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ggplot(mtcars, aes(mpg, wt, size = cyl)) + 
-  grid_panel(func) + 
-  theme_bw() +
-  labs(title = "{svgparser} + {gggrid}: Custom ggplot points with SVG")
-
-
 grid.newpage()
 tree.ht = 0.75
 crn.ht = .25
@@ -58,12 +19,9 @@ tree2 <- grobTree(g.conifer.crown, g.trunk)
 treedist <- data.frame(cbind(x=c(0,1,5,4),y=c(0,.5,3,0)))
 grid.draw(tree2)
 
-vp <- viewport(x=unit(0,'npc'), y=unit(.5,'npc'), width=unit(1,'npc'), height=unit(1,'npc'))
-tree <- grobTree(g.crown, g.trunk, vp=vp)
 
-ggplot()+
-  geom_point(aes(x=x,y=y), data = treedist)+
-  grid_panel(grob = tree)
+
+
 
 
 maketree <- function(treedist){
@@ -149,20 +107,24 @@ ntrees <-  function(totalcover,crown.width, transect.length){
   numberoftrees = log(1-totalcover)/log(1-crown.width/transect.length)
 return(numberoftrees)}
 
-maketree1 <- function(tree.height, branch.height, crown.width, diameter){
+maketree1 <- function(tree.height, branch.height, crown.width, diameter,form.choice,scale.exponent){
   
 
-  
-  
-  
+  #form.choice='E'
+  forms = c('E','D','B','N','G','F1','F2','EX','Dx','GX','FX')
+  f.color = c('darkgreen','green','darkgreen','yellow','yellow','green','darkgreen','gray','gray','gray','gray')
+  use.color = f.color[which(forms == form.choice)]
+  scxp <- scale.exponent#scxp=1
   tree.ht=tree.height/tree.height
-  crn.ht=branch.ht/tree.height
+  crn.ht=branch.height/tree.height
   crn.wd=crown.width/tree.height
   tree.wd=diameter/100/tree.height
   
   
+  g.E.crown <- polygonGrob(x=c(0.5-crn.wd/2, 0.5, 0.5+crn.wd/2), 
+                                 y=c(crn.ht, tree.ht, crn.ht)^scxp, gp=gpar(fill=use.color,alpha=0.5))
   
-  g.crown.round <- polygonGrob(x=c(0.5-crn.wd/2,
+  g.D.crown <- polygonGrob(x=c(0.5-crn.wd/2,
                                    0.5-crn.wd/2,
                                    0.5-0.75*crn.wd/2,
                                    0.5-0.5*crn.wd/2,
@@ -183,9 +145,22 @@ maketree1 <- function(tree.height, branch.height, crown.width, diameter){
                                    (crn.ht*0.05+tree.ht*0.95),
                                    (crn.ht*0.15+tree.ht*0.85),
                                    (crn.ht*0.33+tree.ht*0.67),
-                                   crn.ht), gp=gpar(fill='green',alpha=0.5))
-  g.trunk <- rectGrob(x=0.5, y=(crn.ht-0)/2, width=tree.wd, height=(crn.ht-0), gp=gpar(fill='brown'))
+                                   crn.ht)^scxp, gp=gpar(fill=use.color,alpha=0.5))
+
+  g.trunk <- rectGrob(x=0.5, y=(crn.ht-0)/2, width=tree.wd, height=(crn.ht-0)^scxp, gp=gpar(fill='brown'))
   return(grobTree(g.crown.round, g.trunk))
+  
+  g.G.crown <- polygonGrob(x=c(0.425,0.375,0.475,0.5,0.525,0.625,0.575), 
+                           y=c(0,0.85,0,1,0,0.85,0)^scxp, gp=gpar(fill='yellow',alpha=0.5))
+  
+  g.F1.crown <- polygonGrob(x=c(0.46, 0.46, 0.18, 0.12, 0.05, 0, 0.05, 0.12, 0.18, 0.46, 0.46, 0.37, 0.38, 0.5, 0.6, 0.62, 0.53, 0.54, 0.79, 0.84, 0.94, 1, 0.93, 0.84, 0.8, 0.54, 0.55, 0.46), 
+                            y=c(0, 0.39, 0.57, 0.52, 0.52, 0.61, 0.7, 0.7, 0.61, 0.45, 0.78, 0.82, 0.95, 1, 0.95, 0.82, 0.78, 0.45, 0.68, 0.76, 0.77, 0.68, 0.59, 0.59, 0.63, 0.39, 0, 0)^scxp, gp=gpar(fill=use.color,alpha=0.5))
+  g.F2.crown <- polygonGrob(x=c(0.41, 0.38, 0.34, 0.28, 0.19, 0, 0.22, 0.38, 0.35, 0.4, 0.44, 0.44, 0.37, 0.45, 0.49, 0.56, 0.52, 0.5, 0.53, 0.45, 0.46, 0.52, 0.6, 0.59, 0.74, 1, 0.77, 0.67, 0.61, 0.53, 0.47, 0.41), 
+                            y=c(0, 0.13, 0.2, 0.1, 0.29, 0.41, 0.43, 0.32, 0.24, 0.15, 0.02, 0.38, 0.44, 0.88, 0.96, 1, 0.94, 0.85, 0.42, 0.38, 0.03, 0.16, 0.28, 0.4, 0.45, 0.44, 0.27, 0.12, 0.25, 0.13, 0, 0)^scxp, gp=gpar(fill=use.color,alpha=0.5))
+  grid.newpage()
+  grid.draw(g.F2.crown)
+  
+  c(0.425,0.375,0.475,0.5,0.525,0.625,0.575)-.5
 }
 
 tree.height=30
@@ -223,80 +198,68 @@ ggplot() +
 
 
 
-# remotes::install_github('https://github.com/pmur002/gggrid')
-library(grid)
-library(gggrid)
-
-hex <- FALSE
-if (hex) {
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Create the template grob - a hex!
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  r     <- 0.5
-  theta <- seq(30, 360, 60) * pi/180
-  x     <- r * cos(theta)
-  y     <- r * sin(theta)
-  g     <- grid::polygonGrob(x = x, y = y, vp = viewport(), gp = gpar())
-} else {
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Create the template grob - a 5 pointed star
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  r     <- 0.5
-  theta <- seq(36, 360, 72) * pi/180
-  x     <- r * cos(theta)
-  y     <- r * sin(theta)
-  g     <- grid::polygonGrob(x = x[c(1,3,5,2,4)], y = y[c(1,3,5,2,4)], vp = viewport(), gp = gpar())
+maketree2 <- function(tree.height, branch.height, crown.width, diameter){
+  
+  
+  
+  
+  
+  tree.ht=tree.height/tree.height
+  crn.ht=branch.height/tree.height
+  crn.wd=crown.width/tree.height
+  tree.wd=diameter/100/tree.height
+  
+  
+  
+  g.crown.round <- polygonGrob(x=c(0.5-crn.wd/2,
+                                   0.5-crn.wd/2,
+                                   0.5-0.75*crn.wd/2,
+                                   0.5-0.5*crn.wd/2,
+                                   0.5-0.25*crn.wd/2,
+                                   0.5,
+                                   0.5+0.25*crn.wd/2,
+                                   0.5+0.5*crn.wd/2,
+                                   0.5+0.75*crn.wd/2,
+                                   0.5+crn.wd/2,
+                                   0.5+crn.wd/2), 
+                               y=c(crn.ht,
+                                   (crn.ht*0.33+tree.ht*0.67),
+                                   (crn.ht*0.15+tree.ht*0.85),
+                                   (crn.ht*0.05+tree.ht*0.95),
+                                   (crn.ht*0.02+tree.ht*0.98),
+                                   tree.ht, 
+                                   (crn.ht*0.02+tree.ht*0.98),
+                                   (crn.ht*0.05+tree.ht*0.95),
+                                   (crn.ht*0.15+tree.ht*0.85),
+                                   (crn.ht*0.33+tree.ht*0.67),
+                                   crn.ht)^0.5, gp=gpar(fill='green',alpha=0.5))
+  g.trunk <- rectGrob(x=0.5, y=((crn.ht-0)^0.5)/2, width=tree.wd, height=(crn.ht-0)^0.5, gp=gpar(fill='brown'))
+  return(grobTree(g.crown.round, g.trunk))
 }
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Define the callback function to use within each panel.
-# See https://www.stat.auckland.ac.nz/~paul/Reports/gggrid/gggrid.html
-#
-# @param data data.frame of original data
-# @param coords data.frame of values scaled to fit into the desired output units
-#               as well as any aesthetic values. 
-#               One row per point.
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-panel_func <- function(data, coords) {
-  
-  # Function which will be called for each row in 'coords'
-  position_grob <- function(point) {
-    gnew           <- g
-    gnew$gp$fill   <- point$fill
-    gnew$gp$col    <- NA  #point$colour
-    gnew$gp$alpha  <- point$alpha
-    gnew$vp$x      <- unit(point[['x']], 'npc')
-    gnew$vp$y      <- unit(point[['y']], 'npc')
-    gnew$vp$width  <- unit(point[['size']]/40, 'npc')
-    gnew$vp$height <- unit(point[['size']]/40, 'npc')
-    gnew$name <- strftime(Sys.time(), "%H%M%OS6") # Enforce unique name per grob. 
-    gnew
-  }
-  
-  # For each point, generate a grob
-  grobs <- lapply(
-    seq(nrow(coords)), 
-    function(idx) {
-      position_grob(coords[idx, ])
-    })
-  
-  # group all the point grobs into a single grobTree object
-  do.call(grid::grobTree, grobs)
-}
+tree2 <- maketree2(tree.height, branch.height, crown.width, diameter)
+grid.newpage()
+grid.draw(tree2)
+
+n=round(ntrees(total.cover, crown.width, 50),0)
+
+x = sample(0:50, n)
+rando =  rnorm(n)
+height = rando*0.05*tree.height+tree.height-0.05/2*tree.height
+width = rando*0.05*crown.width+crown.width
+y = height/2
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Plot wih a `gggrid::grid_panel()`
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ggplot(
-  data = mtcars, 
-  mapping = aes(
-    x      = mpg, 
-    y      = wt, 
-    size   = cyl, 
-    fill   = as.factor(cyl)
-  )
-) + 
-  gggrid::grid_panel(panel_func) + 
-  theme_bw() +
-  labs(title = "{gggrid}: Custom ggplot points")
+grobs.tb <- tibble(x = x, y =(y*2)^0.5/2,
+                   width = width/50,
+                   height =  ((height)/50)^0.5,
+                   grob = list(tree2))
+
+ggplot() +
+  geom_grob(data = grobs.tb, 
+            aes(x, y, label = grob, vp.width = width, vp.height = height)
+  ) +
+  scale_y_continuous(name='canopy height (m)',breaks=(c(0:4,(1:25)*5))^0.5,labels=c(0:4,(1:25)*5)) +
+  scale_x_continuous(breaks=c(0:25)*5) +
+  coord_fixed(ratio = 50/50^0.5, ylim = c(0,50^0.5), xlim = c(0,50), expand = F)+
+  theme_bw(12)
