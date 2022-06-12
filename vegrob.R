c53f1522d574187871e4809541d8638931514837
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2022-06-12 19:59:56 -0400

    Update vegrob.R

--- vegrob.R
+++ vegrob.R
@@ -1,4 +1,8 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # remotes::install_github('https://github.com/pmur002/gggrid')
+# install.package('remotes')
+remotes::install_github('coolbutuseless/cssparser') # Handles CSS styling
+remotes::install_github('coolbutuseless/svgparser')
 library(grid)
 library(gggrid)
 library(svgparser)
@@ -6,6 +10,8 @@ library(svgparser)
 library(grid)
 library(ggpmisc)
 library(dplyr)
+
+
 
 
 ntrees <-  function(totalcover,crown.width, transect.length){
@@ -49,7 +55,7 @@ make.tree <- function(tree.height, branch.height, crown.width, diameter,form.cho
                                    (crn.ht*0.15+tree.ht*0.85),
                                    (crn.ht*0.33+tree.ht*0.67),
                                    crn.ht)^scxp, gp=gpar(fill=use.color,alpha=0.5))
-  
+
   g.Ds.crown <- polygonGrob(x=c(-1,
                                -0.9,
                                -0.75,
@@ -60,8 +66,8 @@ make.tree <- function(tree.height, branch.height, crown.width, diameter,form.cho
                                0.5,
                                0.75,
                                0.9,
-                               1)/2+0.5, 
-                           y=(c(0, 
+                               1)/2+0.5,
+                           y=(c(0,
                                 0.5,
                                 0.7,
                                 0.9,
@@ -206,6 +212,111 @@ rm(tree1,tree2,tree3,tree4)
 
 
 
+
+
+
+
+
+
+
+blob <- svgparser::read_svg('blob.svg')
+
+blob2 <- grImport2::grid.picture('blob.svg')
+grid.newpage();grid.draw(blob)
+blob.red <- grid::editGrob(blob, vp = viewport(x=.5,y=.5,width=.15, height=.15), gp = gpar(col="black", alpha=0.5))
+
+blob.red <- svgparser::read_svg('blob.svg', style_default = list(fill = 'red'))
+grid.newpage();grid.draw(blob.red)
+
+
+
+blob <- svgparser::read_svg('blob.svg', obj_type = 'data.frame')
+
+blob.x <- (blob$x - min(blob$x)) / (max(blob$x) - min(blob$x)) 
+blob.y <- 1-1*(blob$y - min(blob$y)) / (max(blob$y) - min(blob$y)) 
+g.D.crown <- polygonGrob(x=blob.x, 
+                         y=blob.y*.25+.75, gp=gpar(fill='green',alpha=0.5))
+grid.newpage();grid.draw(g.D.crown)
+
+make.tree <- function(tree.height, branch.height, crown.width, diameter,form.choice,scale.exponent){
+  #form.choice='E'
+  forms = c('E','D','Ds','B','Bs','N','G','F1','F2','EX','Dx','Dsx','GX','FX')
+  f.color = c('darkgreen','green','green','darkgreen','darkgreen','yellow','yellow','green','darkgreen','gray','gray','gray','gray','gray')
+  use.color = f.color[which(forms == form.choice)]
+  scxp <- scale.exponent#scxp=1
+  tree.ht=tree.height/tree.height
+  crn.ht=branch.height/tree.height
+  crn.wd=crown.width/tree.height
+  tree.wd=diameter/100/tree.height
+  
+  
+  g.E.crown <- polygonGrob(x=c(0.5-crn.wd/2, 0.5, 0.5+crn.wd/2), 
+                           y=c(crn.ht, tree.ht, crn.ht)^scxp, gp=gpar(fill=use.color,alpha=0.5))
+  
+  g.D.crown <- polygonGrob(x=(blob.x*crn.wd)+crn.wd/2, 
+                           y=((blob.y*(1-crn.ht))+crn.ht)^scxp, gp=gpar(fill=use.color,alpha=0.5))
+  
+  g.Ds.crown <- polygonGrob(x=c(-1,
+                                -0.9,
+                                -0.75,
+                                -0.5,
+                                -0.25,
+                                0,
+                                0.25,
+                                0.5,
+                                0.75,
+                                0.9,
+                                1)/2+0.5,
+                            y=(c(0,
+                                 0.5,
+                                 0.7,
+                                 0.9,
+                                 0.95,
+                                 1.00,
+                                 0.95,
+                                 0.9,
+                                 0.7,
+                                 0.5,
+                                 0)*.67+0.33)^scxp, gp=gpar(fill=use.color,alpha=0.5))
+  
+  
+  
+  g.trunk <- rectGrob(x=0.5, y=((crn.ht-0)^scxp)/2, width=tree.wd, height=(crn.ht-0)^scxp, gp=gpar(fill='brown'))
+  
+  g.stems <- polygonGrob(x=c(-0.075,-0.13,-0.12,-0.025,-0.005,0.005,0.025,0.12,0.13,0.075)+0.5, 
+                         y=(c(0,1,1,0,1,1,0,1,1,0)*0.33)^scxp, gp=gpar(fill='brown'))
+  
+  
+  g.G.crown <- polygonGrob(x=c(0.425,0.375,0.475,0.5,0.525,0.625,0.575), 
+                           y=c(0,0.85,0,1,0,0.85,0)^scxp, gp=gpar(fill=use.color,alpha=0.5))
+  
+  g.F1.crown <- polygonGrob(x=c(0.46, 0.46, 0.18, 0.12, 0.05, 0, 0.05, 0.12, 0.18, 0.46, 0.46, 0.37, 0.38, 0.5, 0.6, 0.62, 0.53, 0.54, 0.79, 0.84, 0.94, 1, 0.93, 0.84, 0.8, 0.54, 0.55, 0.46), 
+                            y=c(0, 0.39, 0.57, 0.52, 0.52, 0.61, 0.7, 0.7, 0.61, 0.45, 0.78, 0.82, 0.95, 1, 0.95, 0.82, 0.78, 0.45, 0.68, 0.76, 0.77, 0.68, 0.59, 0.59, 0.63, 0.39, 0, 0)^scxp, gp=gpar(fill=use.color,alpha=0.5))
+  g.F2.crown <- polygonGrob(x=c(0.41, 0.38, 0.34, 0.28, 0.19, 0, 0.22, 0.38, 0.35, 0.4, 0.44, 0.44, 0.37, 0.45, 0.49, 0.56, 0.52, 0.5, 0.53, 0.45, 0.46, 0.52, 0.6, 0.59, 0.74, 1, 0.77, 0.67, 0.61, 0.53, 0.47, 0.41), 
+                            y=c(0, 0.13, 0.2, 0.1, 0.29, 0.41, 0.43, 0.32, 0.24, 0.15, 0.02, 0.38, 0.44, 0.88, 0.96, 1, 0.94, 0.85, 0.42, 0.38, 0.03, 0.16, 0.28, 0.4, 0.45, 0.44, 0.27, 0.12, 0.25, 0.13, 0, 0)^scxp, gp=gpar(fill=use.color,alpha=0.5))
+  
+  if(form.choice %in% c('E','N')){
+    g.tree = grobTree(g.E.crown, g.trunk)
+  }
+  if(form.choice %in% c('D','B')){
+    g.tree = grobTree(g.D.crown, g.trunk)
+  }
+  if(form.choice %in% c('Ds','Bs')){
+    g.tree = grobTree(g.Ds.crown, g.stems)
+  }
+  if(form.choice %in% c('G','GX')){
+    g.tree = grobTree(g.G.crown)
+  }
+  if(form.choice %in% c('F1','FX')){
+    g.tree = grobTree(g.F1.crown)
+  }
+  if(form.choice %in% c('F2')){
+    g.tree = grobTree(g.F2.crown)
+  }
+  
+  return(g.tree) 
+  
+}
 
 
 
