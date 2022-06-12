8436aa2a0d90c3021bf1c16a4514a2579a3a9701
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2022-06-12 16:58:01 -0400

    Update vegrob.R

--- vegrob.R
+++ vegrob.R
@@ -2,106 +2,11 @@
 library(grid)
 library(gggrid)
 library(svgparser)
-
-grid.newpage()
-tree.ht = 0.75
-crn.ht = .25
-crn.wd = 0.25
-tree.wd = 0.025
-# maketree <- function(treedist)
-g.conifer.crown <- polygonGrob(x=c(0.5-crn.wd/2, 0.5, 0.5+crn.wd/2), 
-                               y=c(crn.ht, tree.ht, crn.ht), gp=gpar(fill='darkgreen'))
-g.crown <- roundrectGrob(x=0.5, y=(tree.ht+crn.ht)/2, width=crn.wd, height=(tree.ht-crn.ht), gp=gpar(fill='green'))
-g.trunk <- rectGrob(x=0.5, y=(crn.ht-0)/2, width=tree.wd, height=(crn.ht-0), gp=gpar(fill='brown'))
-tree <- grobTree(g.crown, g.trunk)
-tree2 <- grobTree(g.conifer.crown, g.trunk)
-
-treedist <- data.frame(cbind(x=c(0,1,5,4),y=c(0,.5,3,0)))
-grid.draw(tree2)
-
-
-
-
-
-
-maketree <- function(treedist){
-  g.crown <- roundrectGrob(x=treedist$x, y=(tree.ht+crn.ht)/2+treedist$y, width=crn.wd, height=(tree.ht-crn.ht), gp=gpar(fill='green'))
-  g.trunk <- rectGrob(x=treedist$x, y=(crn.ht-0)/2+treedist$x, width=tree.wd, height=(crn.ht-0), gp=gpar(fill='brown'))
-  tree <- grobTree(g.crown, g.trunk)
-  return(tree)
-}
-
-g.crown <- roundrectGrob(x=unit(0.5,'npc'), y=unit((tree.ht+crn.ht)/2,'npc'), 
-                         width=unit(crn.wd,'npc'), height=unit(tree.ht-crn.ht,'npc'), gp=gpar(fill='green'))
-g.trunk <- rectGrob(x=unit(0.5,'npc'), y=unit((crn.ht-0)/2,'npc'), 
-                    width=unit(tree.wd,'npc'), height=unit(crn.ht-0,'npc'), gp=gpar(fill='brown'))
-
-
-vp <- viewport(x=unit(0/5,'npc'), y=unit(.5/5,'npc'), width=unit(5,'cm'), height=unit(5,'cm'))
-tree <- grobTree(g.crown, g.trunk)
-
-ggplot()+
-  geom_point(aes(x=x,y=y), data = treedist)+
-  coord_cartesian(xlim = c(0,5),ylim = c(0,5)) +
-  grid_panel(tree)
 #https://cran.r-project.org/web/packages/ggpp/vignettes/grammar-extensions.html#geom_grob
 library(grid)
 library(ggpmisc)
 library(dplyr)
 
-tree.ht = 1
-crn.ht = .25
-crn.wd = .5
-tree.wd = 0.025
-
-g.crown <- roundrectGrob(x=unit(0.5,'npc'), y=unit((tree.ht+crn.ht)/2,'npc'), 
-                         width=unit(crn.wd,'npc'), height=unit(tree.ht-crn.ht,'npc'), gp=gpar(fill='green',alpha=0.5))
-
-g.trunk <- rectGrob(x=unit(0.5,'npc'), y=unit((crn.ht-0)/2,'npc'), 
-                    width=unit(tree.wd,'npc'), height=unit(crn.ht-0,'npc'), gp=gpar(fill='brown'))
-
-tree <- grobTree(g.crown, g.trunk)
-
-g.conifer.crown <- polygonGrob(x=c(0.5-crn.wd/2, 0.5, 0.5+crn.wd/2), 
-                               y=c(crn.ht, tree.ht, crn.ht), gp=gpar(fill='darkgreen',alpha=0.5))
-
-g.trunk <- rectGrob(x=0.5, y=(crn.ht-0)/2, width=tree.wd, height=(crn.ht-0), gp=gpar(fill='brown'))
-
-tree2 <- grobTree(g.conifer.crown, g.trunk)
-
-g.crown.round <- polygonGrob(x=c(0.5-crn.wd/2,
-                                 0.5-crn.wd/2,
-                                 0.5-0.75*crn.wd/2,
-                                 0.5-0.5*crn.wd/2,
-                                 0.5-0.25*crn.wd/2,
-                                 0.5,
-                                 0.5+0.25*crn.wd/2,
-                                 0.5+0.5*crn.wd/2,
-                                 0.5+0.75*crn.wd/2,
-                                 0.5+crn.wd/2,
-                                 0.5+crn.wd/2), 
-                             y=c(crn.ht,
-                                 (crn.ht*0.33+tree.ht*0.67),
-                                 (crn.ht*0.15+tree.ht*0.85),
-                                 (crn.ht*0.05+tree.ht*0.95),
-                                 (crn.ht*0.02+tree.ht*0.98),
-                                 tree.ht, 
-                                 (crn.ht*0.02+tree.ht*0.98),
-                                 (crn.ht*0.05+tree.ht*0.95),
-                                 (crn.ht*0.15+tree.ht*0.85),
-                                 (crn.ht*0.33+tree.ht*0.67),
-                                 crn.ht), gp=gpar(fill='green',alpha=0.5))
-
-
-tree3 <- grobTree(g.crown.round, g.trunk)
-
-sample(1:50, 10)
-c=0.19
-p=.1
-n=2
-c = 1-(1-p)^n
-1-c = (1-p)^n
-log(1-c) = n*log(1-p)
 
 ntrees <-  function(totalcover,crown.width, transect.length){
   numberoftrees = log(1-totalcover)/log(1-crown.width/transect.length)
@@ -170,9 +75,9 @@ make.tree <- function(tree.height, branch.height, crown.width, diameter,form.cho
   
     
 
-  g.trunk <- rectGrob(x=0.5, y=(crn.ht-0)/2, width=tree.wd, height=(crn.ht-0)^scxp, gp=gpar(fill='brown'))
+  g.trunk <- rectGrob(x=0.5, y=((crn.ht-0)^scxp)/2, width=tree.wd, height=(crn.ht-0)^scxp, gp=gpar(fill='brown'))
   
-  g.stems <- polygonGrob(x=c(-0.075,-0.13,-0.12,-0.025,-0.005,0.005,0.025,0.12,0.13,0.075)+.5, 
+  g.stems <- polygonGrob(x=c(-0.075,-0.13,-0.12,-0.025,-0.005,0.005,0.025,0.12,0.13,0.075)+0.5, 
                                   y=(c(0,1,1,0,1,1,0,1,1,0)*0.33)^scxp, gp=gpar(fill='brown'))
     
   
@@ -202,7 +107,7 @@ make.tree <- function(tree.height, branch.height, crown.width, diameter,form.cho
   if(form.choice %in% c('F2')){
     g.tree = grobTree(g.F2.crown)
   }
- 
+  
    return(g.tree) 
 
 }
@@ -218,12 +123,12 @@ make.stratum <- function(veg.grob, tree.height, branch.height, crown.width, diam
   grobs.tb <- tibble(x = x, y = (y^scale.exponent)/2,
                      width = width/50,
                      height =  (height/50)^scale.exponent,
-                     grob = list(tree2))
+                     grob = list(veg.grob))
   return(grobs.tb)
 }
 
 
-scale.exponent =1 
+scale.exponent = 0.5 
 tree.height=30
 branch.height=20
 crown.width=15
@@ -251,7 +156,7 @@ total.cover = 0.20
 tree2 <- make.tree(tree.height, branch.height, crown.width, diameter,form.choice,scale.exponent)
 stratum2 <- make.stratum(tree2, tree.height, branch.height, crown.width, diameter, scale.exponent)
 #stat3 ----
-tree.height=2
+tree.height=1.2
 branch.height=1
 crown.width=tree.height
 diameter=1
@@ -306,82 +211,3 @@ rm(tree1,tree2,tree3,tree4)
 
 
 
-
-
-
-ggplot() +
-  geom_grob(data = grobs.tb, 
-            aes(x, y, label = grob, vp.width = width, vp.height = height)
-            ) +
-  scale_y_continuous(name='canopy height (m)',breaks=c(0:25)*5) +
-  scale_x_continuous(breaks=c(0:25)*5) +
-  coord_fixed(ylim = c(0,50), xlim = c(0,50), expand = F)+
-  theme_bw(12)
-
-
-
-maketree2 <- function(tree.height, branch.height, crown.width, diameter){
-  
-  
-  
-  
-  
-  tree.ht=tree.height/tree.height
-  crn.ht=branch.height/tree.height
-  crn.wd=crown.width/tree.height
-  tree.wd=diameter/100/tree.height
-  
-  
-  
-  g.crown.round <- polygonGrob(x=c(0.5-crn.wd/2,
-                                   0.5-crn.wd/2,
-                                   0.5-0.75*crn.wd/2,
-                                   0.5-0.5*crn.wd/2,
-                                   0.5-0.25*crn.wd/2,
-                                   0.5,
-                                   0.5+0.25*crn.wd/2,
-                                   0.5+0.5*crn.wd/2,
-                                   0.5+0.75*crn.wd/2,
-                                   0.5+crn.wd/2,
-                                   0.5+crn.wd/2), 
-                               y=c(crn.ht,
-                                   (crn.ht*0.33+tree.ht*0.67),
-                                   (crn.ht*0.15+tree.ht*0.85),
-                                   (crn.ht*0.05+tree.ht*0.95),
-                                   (crn.ht*0.02+tree.ht*0.98),
-                                   tree.ht, 
-                                   (crn.ht*0.02+tree.ht*0.98),
-                                   (crn.ht*0.05+tree.ht*0.95),
-                                   (crn.ht*0.15+tree.ht*0.85),
-                                   (crn.ht*0.33+tree.ht*0.67),
-                                   crn.ht)^0.5, gp=gpar(fill='green',alpha=0.5))
-  g.trunk <- rectGrob(x=0.5, y=((crn.ht-0)^0.5)/2, width=tree.wd, height=(crn.ht-0)^0.5, gp=gpar(fill='brown'))
-  return(grobTree(g.crown.round, g.trunk))
-}
-
-tree2 <- maketree2(tree.height, branch.height, crown.width, diameter)
-grid.newpage()
-grid.draw(tree2)
-
-n=round(ntrees(total.cover, crown.width, 50),0)
-
-x = sample(0:50, n)
-rando =  rnorm(n)
-height = rando*0.05*tree.height+tree.height-0.05/2*tree.height
-width = rando*0.05*crown.width+crown.width
-y = height/2
-
-
-grobs.tb <- tibble(x = x, y =(y*2)^0.5/2,
-                   width = width/50,
-                   height =  ((height)/50)^0.5,
-                   grob = list(tree2))
-
-ggplot() +
-  geom_grob(data = grobs.tb, 
-            aes(x, y, label = grob, vp.width = width, vp.height = height)
-  ) +
-  scale_y_continuous(name='canopy height (m)',breaks=(c(0:4,(1:25)*5))^0.5,labels=c(0:4,(1:25)*5)) +
-  scale_x_continuous(breaks=c(0:25)*5) +
-  coord_fixed(ratio = 50/50^0.5, ylim = c(0,50^0.5), xlim = c(0,50), expand = F)+
-  theme_bw(12)
