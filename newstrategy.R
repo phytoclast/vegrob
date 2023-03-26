a46fdc24ff48ee08e98d88d90d0cfa844c68bb6d
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2023-03-25 21:06:47 -0400

    new shapes

--- newstrategy.R
+++ newstrategy.R
@@ -38,10 +38,15 @@ ggplot()+
 
 
 
+conifer1 <- read.csv('conifer1.csv') |> data.frame(name='conifer1', fill='darkgreen', color='darkgreen')
+conifer2 <- read.csv('conifer2.csv') |> data.frame(name='conifer2', fill='darkgreen', color='darkgreen')
+cloud1 <- read.csv('cloud1.csv') |> data.frame(name='cloud1', fill='green', color='darkgreen')
+flattop <- read.csv('flattop.csv') |> data.frame(name='flattop', fill='green', color='darkgreen')
+flattop2 <- read.csv('flattop2.csv') |> data.frame(name='flattop2', fill='green', color='darkgreen')
 blob <- read.csv('blob.csv') |> data.frame(name='blob', fill='green', color='darkgreen')
 palm <- read.csv('palm.csv') |> data.frame(name='palm', fill='green', color='darkgreen')|> mutate(x=round(x,2),y=round(y,2))
 trunk <- read.csv('trunk.csv') |> data.frame(name='trunk', fill='orange', color='brown')|> mutate(x=round(x,2),y=round(y,2))
-triangle <- data.frame(x=c(-0.5, 0, 0.5),y=c(0, 1, 0), name='triangle', fill='green', color='darkgreen')
+triangle <- data.frame(x=c(-0.5, 0, 0.5),y=c(0, 1, 0), name='triangle', fill='darkgreen', color='darkgreen')
 dome <- data.frame(x=c(-1,
                        -0.9,
                        -0.75,
@@ -90,8 +95,14 @@ make_B_tree <- function(ht.max, ht.min,crwd,dbh){
   tree = rbind(crown, base)
   tree$ptord <- rownames(tree) |> as.numeric()
   return(tree)}
+make_B_tree2 <- function(ht.max, ht.min,crwd,dbh){
+  crown <- flattop2 |> mutate(x=x*crwd, y=y*(ht.max-ht.min)+ht.min)
+  base <- trunk |> mutate(x=x*dbh/100*1.1, y=y*(ht.min)) 
+  tree = rbind(crown, base)
+  tree$ptord <- rownames(tree) |> as.numeric()
+  return(tree)}
 make_shrub <- function(ht.max, ht.min,crwd){
-  crown <- dome |> mutate(x=x*crwd, y=y*(ht.max-ht.min)+ht.min) #|> data.frame(id=1)
+  crown <- cloud1 |> mutate(x=x*crwd, y=y*(ht.max-ht.min)+ht.min) #|> data.frame(id=1)
   base <- sticks |> mutate(x=x*crwd*0.8, y=y*(ht.min)) #|> data.frame(id=2) 
   tree = rbind(crown, base)
   tree$ptord <- rownames(tree) |> as.numeric()
@@ -103,36 +114,50 @@ make_palm <- function(ht.max, ht.min,crwd,dbh){
   tree$ptord <- rownames(tree) |> as.numeric()
   return(tree)}
 make_N_tree <- function(ht.max, ht.min,crwd,dbh){
-  crown <- triangle |> mutate(x=x*crwd, y=y*(ht.max-ht.min)+ht.min) #|> data.frame(id=1)
+  crown <- conifer1 |> mutate(x=x*crwd, y=y*(ht.max-ht.min)+ht.min) #|> data.frame(id=1)
+  base <- trunk |> mutate(x=x*dbh/100*1.1, y=y*(ht.min)) #|> data.frame(id=2) 
+  tree = rbind(crown, base)
+  tree$ptord <- rownames(tree) |> as.numeric()
+  return(tree)}
+make_N_tree2 <- function(ht.max, ht.min,crwd,dbh){
+  crown <- conifer2 |> mutate(x=x*crwd, y=y*(ht.max-ht.min)+ht.min) #|> data.frame(id=1)
   base <- trunk |> mutate(x=x*dbh/100*1.1, y=y*(ht.min)) #|> data.frame(id=2) 
   tree = rbind(crown, base)
   tree$ptord <- rownames(tree) |> as.numeric()
   return(tree)}
 
 tree <- make_B_tree(ht.max=20,ht.min=7.5,crwd = 5,dbh = 30) 
+tree2 <- make_N_tree2(ht.max=30,ht.min=7.5,crwd = 5,dbh = 30) 
 shrub <- make_shrub(ht.max=3,ht.min=1,crwd=2) 
 
-sample1 <- sample(row.names(stand1), size = 20, prob = stand1$wt)
+sample0 <- sample(row.names(stand1), size = 5, prob = stand1$wt)
+stand2 <- stand1 |> mutate(wt = ifelse(rownames(stand1) %in% sample0, 0,wt))
+sample1 <- sample(row.names(stand2), size = 20, prob = stand2$wt)
 stand2 <- stand1 |> mutate(wt = ifelse(rownames(stand1) %in% sample1, 0,wt))
 sample2 <- sample(row.names(stand2), size = 20, prob = stand2$wt)
+stumps0 <- stand1[sample0,]
 stumps1 <- stand1[sample1,]
 stumps2 <- stand1[sample2,]
+stumps0a <- data.frame(id=rownames(stumps0), xp=stumps0$x, z=stumps0$z)
 stumps1a <- data.frame(id=rownames(stumps1), xp=stumps1$x, z=stumps1$z)
 stumps2a <- data.frame(id=rownames(stumps2), xp=stumps2$x, z=stumps2$z)
 
 
-trees <- merge(stumps1a, tree) 
+trees0 <- merge(stumps0a, tree2) 
+trees1 <- merge(stumps1a, tree) 
 shrubs <- merge(stumps2a, shrub) 
-plants <- rbind(trees, shrubs)
+plants <- rbind(trees0, trees1, shrubs)
 
+#randomize sizes and positions
 plants <- plants |> group_by(id) |> 
   mutate(ht.max = max(y), crwd = max(x)-min(x),
-         xpp = xp + runif(1, min = -0.5, max = 0.5),
-         yr = rnorm(1,ht.max, ht.max/5)/ht.max,
-         xr = rnorm(1,ht.max, ht.max/5)/ht.max+rnorm(1,crwd, crwd/5)/crwd,
-         xn = x*xr+xpp,
-         yn = y*yr)
+         xpp = xp + runif(1, min = -0.5, max = 0.5),#shift position on grid
+         yr = rnorm(1,ht.max, ht.max/10)/ht.max,#deviation in height
+         xr = (rnorm(1,ht.max, ht.max/10)/ht.max+rnorm(1,crwd, crwd/10)/crwd)/2,#deviation in width partially related to height
+         xn = x*xr+xpp,#resized width and put on new position
+         yn = y*yr*(1-1/15))#resized height adjusted downward show that variation is less than max height in the field
 
+#rearrange stems depth drawing order
 plants <- plants |> arrange(id, name, ptord, z)
 zmax <- max(plants$z)
 zmin <- min(plants$z)
@@ -142,10 +167,10 @@ plants2 <- plants |> subset(z < zmax-zwid/3 & z >= zmin+zwid/3)
 plants3 <- plants |> subset(z >= zmax-zwid/3)
 
 ggplot() +
-  geom_polygon(data=plants1, aes(x=xn,y=yn,group=paste0(name,id), fill=fill, color=color), alpha=0.8, size=0.01)+
-  geom_polygon(data=plants2, aes(x=xn,y=yn,group=paste0(name,id), fill=fill, color=color), alpha=0.8, size=0.01)+
-  geom_polygon(data=plants3, aes(x=xn,y=yn,group=paste0(name,id), fill=fill, color=color), alpha=0.8, size=0.01)+
-  scale_fill_manual(values=c('green','orange'))+
+  geom_polygon(data=plants1, aes(x=xn,y=yn,group=paste0(name,id), fill=fill, color=color), alpha=0.9, size=0.01)+
+  geom_polygon(data=plants2, aes(x=xn,y=yn,group=paste0(name,id), fill=fill, color=color), alpha=0.9, size=0.01)+
+  geom_polygon(data=plants3, aes(x=xn,y=yn,group=paste0(name,id), fill=fill, color=color), alpha=0.9, size=0.01)+
+  scale_fill_manual(values=c('darkgreen','green','orange'))+
   scale_color_manual(values=c('brown','darkgreen'))+
   theme(legend.position = "none", 
         panel.background = element_rect(fill = rgb(0.85,0.95,1,0.5),
